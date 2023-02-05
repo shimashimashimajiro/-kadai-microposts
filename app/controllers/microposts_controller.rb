54440fb288a8976ca5ba0b0e8636c19e4354b022
shimajiro <saitoua.235@gmail.com>
2023-02-05 14:08:41 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,12 +3,12 @@ class MicropostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def create
-    @micropost = current_user.microposts.build(micropost_params)
+   @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
+      @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
