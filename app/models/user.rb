7ce3abe8755cb656aacc415f9c8ccfa81330615b
shimajiro <saitoua.235@gmail.com>
2023-02-14 04:30:32 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :submicroposts, through: :favorites ,source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
@@ -29,5 +34,21 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  #登録メソッド 
+  def favorite(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  #登録解除メソッド
+  def unfavorite(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  #確認メソッド
+  def checkfavorite?(micropost)
+    self.submicroposts.include?(micropost)
   end
 end
